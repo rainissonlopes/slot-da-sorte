5454c7cf92daa8d34cbf60bb2d60f555a687812c
rainissonlopes <lopesrainisson@gmail.com>
2026-05-24 16:10:02 -0400

    corrige analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
